6aca5adbbab3e47d30f1ed1bf9676cdf257b4e8a
Dave Young <solenovex@live.com>
2017-10-11 21:15:09 +0800

    addStep3

---
+++ Step 3/CoreBackend.Api/Startup.cs
@@ -0,0 +1,43 @@
+﻿using CoreBackend.Api.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using NLog.Extensions.Logging;
+
+namespace CoreBackend.Api
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+#if DEBUG
+            services.AddTransient<IMailService, LocalMailService>();
+#else
+            services.AddTransient<IMailService, CloudMailService>();
+#endif
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            // loggerFactory.AddProvider(new NLogLoggerProvider());
+            loggerFactory.AddNLog();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler();
+            }
+
+            app.UseStatusCodePages();
+
+            app.UseMvc();
+        }
+    }
+}
